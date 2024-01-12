17ba14cd4e12bba041e8b7f38e0728bea15d2f86
Joe Jing <joe_jing_91@163.com>
2024-01-12 11:00:06 +0800

    string 1

---
+++ string/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+unsigned int  trim_space(string &s) {
+	unsigned int fast = 0;
+	unsigned int slow = 0;
+	for(; fast < s.length(); fast++) {
+		if(s[fast] != ' ') {
+			s[slow] = s[fast];
+			slow++;
+		} else {
+			if(slow == 0) {
+				continue;
+			}
+			if(s[slow - 1] == ' ') {
+				continue;
+			} else {
+				s[slow] = s[fast];
+				slow++;
+			}
+		}
+	}
+	return  slow;
+}
+
+void revert_word(string &s , unsigned int start, unsigned int len) {
+	unsigned int left = 0 + start;
+	unsigned int right = len - 1 + start;
+	char tmp;
+	unsigned int mid = len / 2 + start;
+	for(; left < mid; left++, right--) {
+		tmp = s[left];
+		s[left] = s[right];
+		s[right] = tmp;
+	}
+}
+
+void revert_sentence(string &s) {
+	revert_word(s, 0, s.length());
+}
+
+void revert_words(string &s) {
+	revert_sentence(s);
+	s.resize(trim_space(s));
+	unsigned int i;
+	unsigned int start = 0;
+	for(i = 0; i < s.length(); i++) {
+		if(s[i] != ' ') {
+			continue;
+		} else {
+			revert_word(s, start, i - start);
+			i++;
+			start = i;	
+		}
+	}
+	revert_word(s, start, i - start);
+}	
+
+int main(void) {
+	string s = " this is  a  example ";
+	revert_words(s);
+	cout << s << endl;
+	return 0;
+}
